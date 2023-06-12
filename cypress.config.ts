c11a128df1425f029c2925ab06b60fb8bd8beb9f
Samuel <samuelalves681@gmail>
2023-06-11 21:02:38 -0300

    fix lint errors

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     video: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 });
